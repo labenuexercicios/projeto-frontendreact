7da904ff374fb6d8c0f39ba613b7882304a29a0d
mbiga <mbigaram.mb@gmail.com>
2022-10-24 08:52:59 -0300

    projeto-react

---
+++ projeto-front-end-react/src/Produtos/Produtos.js
@@ -0,0 +1,20 @@
+// import React from "react";
+import { MainContainer } from './styles'
+
+
+
+
+export default function Produtos(){
+
+    const produto1 = "Foguete 1"
+    const produto2 = "Satélite 1"
+
+    return(
+        <MainContainer>
+        <section>{produto1}</section>
+        <section>{produto2}</section>
+        </MainContainer>
+
+    );
+
+}
