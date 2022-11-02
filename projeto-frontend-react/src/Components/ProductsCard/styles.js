1712776be5e0441ac7e4dc9c0ffbf61b4fb9c976
alinekabbas <alinekabbas@gmail.com>
2022-11-02 17:05:26 -0300

    feat: projeto react em andamento

---
+++ projeto-frontend-react/src/Components/ProductsCard/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+    width: 250px;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border: 1px solid black;
+
+`
