8d490cae5b56c3814915ba98243eba45f423836e
alinekabbas <alinekabbas@gmail.com>
2022-11-16 17:39:58 -0300

    feat: projeto em andamento

--- src/Components/ProductsCard/styles.js
+++ src/Components/ProductsCard/styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
+import imageCards from "../../assets/backgroundCards.png"
 
 export const Card = styled.div`
-    width: 240px;
-    height: 380px;
+    width: 250px;
+    height: 400px;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    border: 1px solid black;
     border-radius: 20px;
     background-color: #012f6d;
     color: white;
