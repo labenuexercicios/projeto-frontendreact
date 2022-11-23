8c4902dfcc1fe144590d49796e358a9346c886f3
Aureana <aureana2018@outlook.com>
2022-11-23 17:17:21 -0300

    Atualizações Carrinho e Footer

--- src/Componentes/Carrinho/Carrinho.js
+++ src/Componentes/Carrinho/Carrinho.js
@@ -24,16 +24,13 @@ function Carrinho(props) {
                     { props.listaCarrinho.length === 0? 
                     <p> Carrinho vazio</p> : 
                     props.listaCarrinho
-                        .map((produto) => {
-                            {totalPreco += Number(produto.preco)}
+                        .map((produto) => {                            
+                            {totalPreco += Number(produto.preco)*(produto.quantity)}
                             return(
                             <section className="botoesCarrinho">
                                 <p>Produto escolhido: {produto.produto}</p>
                                 <p>Preço: {formatarPreco.format(produto.preco)}</p>
-                                <p>Qtd: {produto.quantity}</p>
-                                
-        
-        
+                                <p>Qtd: {produto.quantity}</p>        
                                 <button className="removemItem" onClick={() => props.removeBrinquedo(produto.id)}>Remover ítem</button>
                             </section>)
                         })
--- src/Componentes/Footer/styled.js
+++ src/Componentes/Footer/styled.js
@@ -1,8 +1,17 @@
 import styled from "styled-components";
 
 export const FooterDiv = styled.div`
-height: 40px;
+height: 50px;
 background-color: blue;
 border: 2px solid ridge;
+display: flex;
+width: 100%;
+color: white; 
+align-items: center;
+justify-content: center;
+img{
+    width: 20px;
+    height: 20px;   
+}
 
 `
