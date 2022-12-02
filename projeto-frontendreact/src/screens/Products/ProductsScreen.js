840a50585238fd69ca838b4956bf9e6ad995ec9c
Raissa Baeta <rbaetadev@gmail.com>
2022-12-02 11:21:56 -0300

    implementação products.json

--- projeto-frontendreact/src/screens/Products/ProductsScreen.js
+++ projeto-frontendreact/src/screens/Products/ProductsScreen.js
@@ -1,9 +1,31 @@
 import { SideBarContainer } from "./ProductsScreen.styled";
-
+import products from "../../assets/products.json"
+import cartIcon from "../../assets/cart-white.png"
+import { Main } from "./ProductsScreen.styled"
 
 function ProdutcsScreen(props) {
 
+
+
     return (
+
+     <div>
+        {products.map((product)=> (
+         <Main key={product.id}>
+            <img scr={product.imageUrl} alt={product.imageAlt} />
+            <div>
+                <h3>{product.name}</h3>
+                <h4>{product.description}</h4>
+            </div>
+            <div>
+                <span>{product.price}</span>
+                <button>
+                    <img src={cartIcon} alt="cart icon"/>
+                </button>
+            </div>
+         </Main>  
+        ))}
+    
         <SideBarContainer>
             <div className="side-bar">
                 <h3>Preço</h3>
@@ -15,8 +37,8 @@ function ProdutcsScreen(props) {
                     <option value="decrescente">Decrescente</option>
                 </select>
             </div>
-
         </SideBarContainer>
+        </div>
     );
 }
 
--- projeto-frontendreact/src/screens/Products/ProductsScreen.styled.js
+++ projeto-frontendreact/src/screens/Products/ProductsScreen.styled.js
@@ -10,8 +10,10 @@ export const SideBarContainer = styled.div`
     height: 120px;
     background-color: #E6E6E6;
     border-radius: 8px;
-    margin: 20px 50px;
-
+    padding: 6px;
+    position: fixed;
+    left: 10px;
+    top: 180px;
 }
 
 input {
@@ -38,4 +40,35 @@ input {
         border: 1px solid #14b6b9;
         border-radius: 8px;
     }
+`
+
+export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    width: 500px;
+    height: 300px;
+    margin: 0 auto;
+    button {
+        background-color: #14b6b9;
+        border: none;
+        width: 30px;
+        height: 30px;
+        border-radius: 8px;
+
+        :hover {
+            cursor: pointer;
+            filter: brightness(1.1);
+        }
+
+        :active {
+            filter: brightness(0.95);
+        }
+    }
+
+
+
+
 `
