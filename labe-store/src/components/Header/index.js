fedc8e120b871f96d6c7c211a9c195daed0bc76f
Matheus Mendes <matheusmendesreis@outlook.com>
2022-12-16 08:13:07 -0300

    docs: componente Header criado, ainda estilizar.

---
+++ labe-store/src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <button><img src="" alt="logo" /></button>
+            </div>
+            <div>
+                <ul>
+                    <li><button></button></li>
+                    <li><button></button></li>
+                    <li><button></button></li>
+                </ul>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
