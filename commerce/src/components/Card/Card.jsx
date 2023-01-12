15c6d7ca18dec5a94fc92052c219f4892fb7bd15
Edson Aurelio De Oliveira Araujo <edson.araujo@linx.com.br>
2023-01-12 14:51:53 -0300

    Criando o card de produtos

---
+++ commerce/src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CardBox } from "./style";
+
+export default function Card(props) {
+  return(
+    <CardBox>
+      Produto
+    </CardBox>
+  )
+}
--- commerce/src/components/Main/style.jsx
+++ commerce/src/components/Main/style.jsx
@@ -3,3 +3,8 @@ import styled from 'styled-components'
 export const MainContainer = styled.main`
   width: 50vw;
 `
+export const MainProdContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+`
