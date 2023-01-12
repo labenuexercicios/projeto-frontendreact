9b71bbcd33e10b3ab5b677c0150fc2cc9c72da13
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-01-12 02:23:34 -0300

    footer-criado

---
+++ projeto-frontend-rafael-machado/src/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+
+
+            <div className="footer-dados">
+                <div className="footer-introducao">
+                    <img />
+                    <br />
+                </div>
+
+                <div className="footer-contatos">
+                    <p><h3>Fale Conosco:</h3></p><br />
+                    <p><b>Telefone de contato</b></p>
+                    <p>(24)98826-9179
+                    </p>
+                    <br />
+                    <p><b>rafaelmartinsmachado25@gmail.com</b></p>
+                    <br />
+                    <p>Qualquer duvida é só chamar!</p>
+                    <br />
+                </div>
+            </div>
+
+            <div className="footer-rafael">
+
+                <div>
+                    
+                    <a href="https://github.com/RafaelMachado1" >https://github.com/RafaelMachado1"</a>
+                </div>
+                <div>
+                   
+                    <a href="https://www.linkedin.com/in/rafael-m-machado-95305688/" >https://www.linkedin.com/in/rafael-m-machado-95305688/</a>
+                </div>
+
+
+            </div>
+
+        </>
+    )
+}
+
+export default Footer
