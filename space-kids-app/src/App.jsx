e2bb2eda8b9671ada0072a3281477f043eba5d0c
suuzanemoura <[suuzanemoura@gmail.com]>
2023-01-25 23:13:46 -0300

    App react criado com Vite

---
+++ space-kids-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './assets/SpaceKids_Logo.svg'
+
+function App() {
+
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  )
+}
+
+export default App
