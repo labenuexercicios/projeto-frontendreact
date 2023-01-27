e7cc6932a100766859286f0ed5a6ec37d514842c
Gabriel Garuthi <gabrielderu@hotmail.com>
2023-01-27 10:32:47 -0300

    Atualizado

--- tentativa1-project-react/src/App.js
+++ tentativa1-project-react/src/App.js
@@ -7,12 +7,39 @@ import { Teladeitens } from './Componentes/Teladeitens';
 // import './App.css';
 
 function App() {
+  
+  let arraydositens = []
+
+  const [item1, setItem1] = useState({
+    nome: "Jinko",
+    geracao: 450,
+    saf: "Sim",
+    preco: 14000.00
+  })
+
+  const [item2, setItem2] = useState({
+    nome: "Canadian",
+    geracao: 465,
+    saf: "Sim",
+    preco: 12500.00
+  })
+
+  const [item3, setItem3] = useState({
+    nome: "Trina",
+    geracao: 565,
+    saf: "Não",
+    preco: 13500.00
+  })
+
+  arraydositens.push(item1, item2, item3);
+  console.log(arraydositens)
 
   const [vlmax, setVlmax] = useState(0)
   const [vlmin, setVlmin] = useState(0)
   const [vlnome, setVlnome] = useState("")
   const [filtromaxTeste, setFiltomaxteste] = useState(0)
   const [filtrominTeste, setFiltominteste]  = useState(0)
+  const [adicionacarrinho, setAdicionacarrinho] = useState(0);
 
 
 
@@ -49,7 +76,15 @@ function App() {
       <Estilodositens>
     {filtromaxTeste === 1 && filtrominTeste === 1  ? (
         <Teladeitens
-
+      item1 = {item1}
+      setItem1 = {setItem1}
+      item2 = {item2}
+      setItem2 = {setItem2}
+      item3 = {item3}
+      setItem3 = {setItem3}
+      adicionacarrinho = {adicionacarrinho}
+      setAdicionacarrinho = {setAdicionacarrinho}
+      arraydositens = {arraydositens}
         />
       ) : (
 
@@ -58,7 +93,13 @@ function App() {
       </Estilodositens>
 
       <Estilodocarrinho>
-        <Carrinho/>
+      {/* {adicionacarrinho === 1 ? (
+           <Carrinho
+           item1 = {item1}
+           item2 = {item2}
+           item3 = {item3}
+           /> 
+      ) : (<p></p>)} */}
       </Estilodocarrinho>
     
     
--- tentativa1-project-react/src/Componentes/Carrinho.js
+++ tentativa1-project-react/src/Componentes/Carrinho.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 
-export function Carrinho(){
+export function Carrinho(props){
+
+  
 
     return(
         <ol>
         <h1>Carrinho:</h1>
-        <br></br>
-        <input type="text" placeholder="Busca" id="campoDeBusca" />
-        <button>Filtrar</button>
+        {/* <br></br> */}
+        <h3>Valor total: {props.item.preco}</h3>
+        {/* <input type="text" placeholder="Busca" id="campoDeBusca" />
+        <button>Filtrar</button> */}
       </ol>
     )
 
