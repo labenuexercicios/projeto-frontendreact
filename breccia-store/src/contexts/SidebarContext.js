c999106d2cc0251399af6dcc707fab9f688cc0f5
nlperri <lnataliaperri@gmail.com>
2023-02-06 17:11:36 -0300

    sidebar issues fixed

--- breccia-store/src/contexts/SidebarContext.js
+++ breccia-store/src/contexts/SidebarContext.js
@@ -5,7 +5,6 @@ export const SidebarContext = createContext();
 
 const SidebarProvider = ({ children }) => {
   const [open, setOpen] = useState(false);
-  const { setFormPage } = useContext(CheckoutContext);
 
   const handleClose = () => {
     setOpen(false);
