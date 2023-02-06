1ee02bedea3d88aa9e21660fe33872cf45ef88e8
azzindr <andreazziarq@gmail.com>
2023-02-06 13:53:54 -0300

    componentização

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Items } from "../Items/Items";
+
+export default function Cart(){
+    return(
+        <div>
+            <p>Cart</p>
+            <Items />
+
+        </div>
+    )
+}
