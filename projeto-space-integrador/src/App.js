154b85bfb27007f579203b17c8e469105670fc23
Rafael <rafa.07.g3@gmail.com>
2023-02-13 15:58:54 -0300

    componentização

--- projeto-space-integrador/src/App.js
+++ projeto-space-integrador/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Filter from './componentes/Filters/filters';
+import Home from './componentes/ProdutcList/Home/home';
+import Cart from './componentes/ProdutcList/ProductCard/productCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter/>
+      <Home/>
+      <Cart/>
+      
     </div>
   );
 }
