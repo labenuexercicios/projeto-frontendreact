ce03a90cd0d78b87416f7fcaa72a8b978b4f61be
botechia-erika <mendoncabotechia@proton.me>
2023-03-04 14:36:30 -0300

    commit

--- projeto-frontend-react-pilares/src/Components/Order/OrderCategories/index.js
+++ projeto-frontend-react-pilares/src/Components/Order/OrderCategories/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { InputSelect , InputOption} from './styled';
-export function OrderCategories({products, setProducts,  filterType}){
+export function OrderCategories({filterType}){
     
     return(
     <div>
         
-    <InputSelect >
+    <InputSelect onChange={filterType} >
         <InputOption value="0">TIPO</InputOption>
         <InputOption value="1">Camiseta</InputOption>  
         <InputOption value="2">Calça</InputOption>
