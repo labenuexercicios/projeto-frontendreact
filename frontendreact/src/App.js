3d996f3408af032dcad17bf0058b9c24643c5853
henriquedias420 <henriquediasper@hotmail.com>
2023-05-03 20:14:48 -0300

    [Configuração] Criei os componentes principais do projeto

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Filters } from './Components/Filters/Filters';
+import { Home } from './Components/ProductList/Home/Home';
+import { ProductCard } from './Components/ProductList/ProductCard/ProductCard';
+import { Cart } from './Components/ShoppingCart/Cart/Cart';
+import { Items } from './Components/ShoppingCart/Items/Items';
 
 function App() {
   return (
     <div className="App">
-      
+      <Filters />
+      <Home />
+      <ProductCard />
+      <Cart />
+      <Items />
     </div>
   );
 }
