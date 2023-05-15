117ec11bb2ffae9f84e74fe08c8d0b30ff90bcbf
henriquedias420 <henriquediasper@hotmail.com>
2023-05-15 16:51:30 -0300

    funcionalidades apliquei inputs controlados

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -4,8 +4,17 @@ import { Home } from "./Components/ProductList/Home/Home";
 import { Cart } from "./Components/ShoppingCart/Cart/Cart";
 import { productList } from "./Components/Assents/productsList";
 import GlobalStyle from "./GlobalStyle";
+import { useState } from "react";
 
 function App() {
+
+  const [minFilter, setMinFilter] = useState("")
+  const [maxFilter, setMaxFilter] = useState("")
+  const [searchFilter, setSearchFilter] = useState("")
+  const [cart, setCart] = useState("")
+  const [amount, setAmount] = useState("")
+
+
   return (
     <AppStyle>
       <GlobalStyle />
