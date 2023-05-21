ea8223167165e1f31ee316e979f423babee4d2ce
MiaAntunes <suportecamilaantunes2003@hotmail.com>
2023-05-21 16:32:43 -0300

    Reestruturação do CSS

--- projeto-labecommerce/src/components/Items/Items.jsx
+++ projeto-labecommerce/src/components/Items/Items.jsx
@@ -3,11 +3,17 @@ import { useState } from "react"
 export function Items({item, removeItem}){
     
     return(
-        <div className="containerItems">
+       <div className="containerItems">
+        <div className="firstContainerItems">
             <input type="text" id="amount" value={item.amount} />
             <p>{item.name}</p>
-            <p> {item.value}</p>
-            <button onClick={()=>removeItem(item)}>Remover</button>
         </div>
+        <div className="secondContainerItems">
+
+            <button onClick={()=>removeItem(item)}>Remover</button>
+            <p>R${item.value},00</p>
+        </div>
+        
+       </div>
     )
 }
