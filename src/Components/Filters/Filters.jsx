9343745ec08ba88802854f81c41de70b6bcc7c36
karinadarc <darcsilva.kd@gmail.com>
2023-05-26 20:13:54 -0300

    Estiliza Filtros

--- src/Components/Filters/Filters.jsx
+++ src/Components/Filters/Filters.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { AsideFiltro, TituloFiltro } from './FiltersStyle'
 
 function Filters() {
   return (
-    <>
-    <p>Filters</p>
-    </>
+    <AsideFiltro>
+    <TituloFiltro>Filters</TituloFiltro>
+    <label>Valor minímo:</label>
+    <input type="text" />
+    <label>Valor máximo:</label>
+    <input type="text" />
+    <label>Busca por nome:</label>
+    <input type="text" />
+    </AsideFiltro>
   )
 }
 
---
+++ src/Components/Filters/FiltersStyle.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const AsideFiltro = styled.aside`
+display:flex;
+border: 1px solid black;
+flex-direction:column;
+padding-left: 10px;
+
+input {
+    width:10%;
+    margin-bottom:10px;  
+}
+`
+export const TituloFiltro = styled.h1`
+   
+`;
+
