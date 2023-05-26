077d5962d25447956bbc3efcbaf1681bd2d5cf87
MiaAntunes <suportecamilaantunes2003@hotmail.com>
2023-05-26 17:12:25 -0300

    Refiz o estilo da página

--- projeto-labecommerce/src/assets/ListaProdutos.js
+++ projeto-labecommerce/src/assets/ListaProdutos.js
@@ -4,49 +4,74 @@ import foto3  from "./img/foto3.png"
 import foto4  from "./img/foto4.png"
 import foto5  from "./img/foto5.png"
 import foto6  from "./img/foto6.png"
+import foto7  from "./img/foto7.png"
+import foto8  from "./img/foto8.png"
+import foto9  from "./img/foto9.png"
 
 export const listaDeProduto = [
     {
         id:1,
-        name:"Ursinho de Pelúcia Pork Sweet",
-        value: 200.00,
+        name:"Boneco Star Trek 12cm - Michelle Burnham",
+        value: 90.00,
         imageUrl: foto1,
         quantity:0
     },
     {
         id:2,
-        name:"Ursinho de Pelúcia Sanduiche Sweet",
+        name:"Nave Star Trek USS Voyager 74656",
         value: 150.00,
         imageUrl: foto2,
         quantity:0
     },
     {
         id:3,
-        name:"Ursinho de Pelúcia Avocado Sweet",
-        value: 350.00,
+        name:"Boneco Star Trek 12cm - Spoke",
+        value: 90.00,
         imageUrl: foto3,
         quantity:0
     },
     {
         id:4,
-        name:"Ursinho de Pelúcia Unicorn Sweet",
+        name:"Lego Doctor Who 12cm - Tardis",
         value: 250.00,
         imageUrl: foto4,
         quantity:0
     },
     {
         id:5,
-        name:"Ursinho de Pelúcia Dog Sweet",
-        value: 300.00,
+        name:"Lego Doctor Who 12cm - Daleks",
+        value: 150.00,
         imageUrl: foto5,
         quantity:0
     },
     {
         id:6,
-        name:"Ursinho de Pelúcia Pokemon Sweet",
-        value: 450.00,
+        name:"Brinquedo Doctor Who 30cm - K9 Mark I",
+        value: 250.00,
         imageUrl: foto6,
         quantity:0
-    }
+    },
+    {
+        id:6,
+        name:"Boneco The Mandalorian 30cm - Baby Yoda",
+        value: 350.00,
+        imageUrl: foto7,
+        quantity:0
+    },
+    {
+        id:6,
+        name:"Boneco Star Wars 15cm - Darth Vader",
+        value: 150.00,
+        imageUrl: foto8,
+        quantity:0
+    },
+    {
+        id:6,
+        name:"Brinquedo Star Wars - Sabre de Luz",
+        value: 200.00,
+        imageUrl: foto9,
+        quantity:0
+    },
+    
 ]
 //Dados mokados
--- projeto-labecommerce/src/components/Cart/index.jsx
+++ projeto-labecommerce/src/components/Cart/index.jsx
@@ -3,61 +3,60 @@ import { Items } from "../Items";
 import { HiShoppingCart } from "react-icons/hi";
 
 export function Cart(props) {
+  const removeItem = (elemento) => {
+    const haveInCart = props.cart.find((item) => item.id === elemento.id);
 
-  const removeItem = (elemento) =>{ 
-    const haveInCart = props.cart.find((item)=> item.id === elemento.id)
-    
-    if(haveInCart.quantity === 1){
-      const updateCart = props.cart.filter((item)=> item.id !== elemento.id)
-      props.setCart(updateCart)
-
-    }else{
+    if (haveInCart.quantity === 1) {
+      const updateCart = props.cart.filter((item) => item.id !== elemento.id);
+      props.setCart(updateCart);
+    } else {
       const updateInCart = props.cart.map((item) => {
-        return {...item, quantity: item.quantity - 1}
-      })
-      props.setCart(updateInCart)
+        return { ...item, quantity: item.quantity - 1 };
+      });
+      props.setCart(updateInCart);
     }
-  }
+  };
 
-  useEffect(()=>{ // Salvar os itens
-    const listCart = JSON.stringify(props.cart)
-    if(props.cart.length > 0){
-      localStorage.setItem('cart', listCart)
+  useEffect(() => {
+    // Salvar os itens
+    const listCart = JSON.stringify(props.cart);
+    if (props.cart.length > 0) {
+      localStorage.setItem("cart", listCart);
     }
-  },[props.cart])
+  }, [props.cart]);
 
-  useEffect(()=>{ // Salvar os valor
-    let listAmount = 0
-    if(props.cart.length){
-      props.cart.map((item)=>{
-        return (listAmount += (item.quantity * item.value))
-      })
-      props.setAmount(listAmount)
-    }else{
-      props.setAmount(0)
+  useEffect(() => {
+    // Salvar os valor
+    let listAmount = 0;
+    if (props.cart.length) {
+      props.cart.map((item) => {
+        return (listAmount += item.quantity * item.value);
+      });
+      props.setAmount(listAmount);
+    } else {
+      props.setAmount(0);
     }
-  },[props.cart])
+  }, [props.cart]);
 
-  useEffect(()=>{
-    const listCart = localStorage.getItem('cart')
-    const listCartArray = JSON.parse(listCart)
-    if(listCart){
-      props.setCart(listCartArray) 
-    }else{
-      props.setCart([])
+  useEffect(() => {
+    const listCart = localStorage.getItem("cart");
+    const listCartArray = JSON.parse(listCart);
+    if (listCart) {
+      props.setCart(listCartArray);
+    } else {
+      props.setCart([]);
     }
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    const listAmount = localStorage.getItem('amount')
-    const listAmountArray = JSON.parse(listAmount)
-    if(listAmount){
-      props.setAmount(listAmountArray)
-    }else{
-      props.setAmount(0)
+  useEffect(() => {
+    const listAmount = localStorage.getItem("amount");
+    const listAmountArray = JSON.parse(listAmount);
+    if (listAmount) {
+      props.setAmount(listAmountArray);
+    } else {
+      props.setAmount(0);
     }
-  },[])
-  
+  }, []);
 
   return (
     <div className="containerCart">
@@ -70,15 +69,17 @@ export function Cart(props) {
         />
       </div>
       <div className="second">
-        {props.cart
-          .map((item, index)=>{
-            return(
+        {props.cart.map((item, index) => {
+          return (
             <>
-                <Items key={index} item={item} removeItem={removeItem} />
+              <Items key={index} item={item} removeItem={removeItem} />
             </>
-          )
+          );
         })}
-        <p id="amount">Valor da Compra: R${props.amount} </p>
+        <div className="second-value">
+          <p className="amount">Valor da Compra: </p>
+          <p className="amount" id="value">R$ {props.amount}</p>
+        </div>
       </div>
     </div>
   );
