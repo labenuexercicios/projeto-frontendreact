e56db7c38e876af2c7d89fba5374adad314dc089
Guilherme <guibarros.dev@gmail.com>
2023-05-26 11:40:53 -0300

    Apliquei inputs controlados no select

--- labecommerce/src/Components/ProductList/ProductCard/ProductCard.jsx
+++ labecommerce/src/Components/ProductList/ProductCard/ProductCard.jsx
@@ -3,6 +3,7 @@ import { DivProduct, DivProduct1 } from "./ProductCardStyle";
 import productList from "../../../assents/productList";
 
 function ProductCard({ productList }) {
+  console.log(productList);
   return (
     <DivProduct>
       <img src="https://picsum.photos/300/335?a=" />
