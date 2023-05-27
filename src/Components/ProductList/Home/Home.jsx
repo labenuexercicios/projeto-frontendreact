17dc1286d50469883600ac3c3efe2dd81cfd128f
karinadarc <darcsilva.kd@gmail.com>
2023-05-26 21:03:00 -0300

    Adiciona Estilização Home

--- src/Components/ProductList/Home/Home.jsx
+++ src/Components/ProductList/Home/Home.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import ProductCard from '../ProductCard/ProductCard'
+import React from "react";
+import ProductCard from "../ProductCard/ProductCard";
+import { HomeArticle, HomeCards, OrdenacaoSection } from "./HomeStyle";
 
 function Home() {
   return (
-    <>
-      <p>Home</p>
-      <ProductCard/>
-    </>
-  )
+    <HomeArticle>
+      <OrdenacaoSection>
+        <div>Quantidade de produtos:</div>
+        <div>
+          <label>Ordenação</label>
+          <select>
+            <option value="">Opções</option>
+          </select>
+        </div>
+      </OrdenacaoSection>
+
+      <HomeCards>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </HomeCards>
+    </HomeArticle>
+  );
 }
 
-export default Home
+export default Home;
