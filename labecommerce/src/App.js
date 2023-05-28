9c14de8865d44714f73f64b418f9c71cd135d467
j0rdanaaa <j0rdanaaa@gmail.com>
2023-05-28 20:33:29 -0300

    componentes principais do projeto

--- labecommerce/src/App.js
+++ labecommerce/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Filters from './components/Filters/Filters'
+import Home from './components/Home/Home'
+import Cart from './components/Cart/Cart'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Filters />
+      <Home />
+      <Cart />
+
     </div>
   );
 }
