1abd1f282314daf772c1bc8963e356e032efdb66
karinadarc <darcsilva.kd@gmail.com>
2023-06-01 22:13:50 -0300

    Implementa inputs controlados

--- src/Components/ProductList/Home/Home.jsx
+++ src/Components/ProductList/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "../ProductCard/ProductCard";
 import { HomeArticle, HomeCards, OrdenacaoSection } from "./HomeStyle";
 
@@ -6,30 +6,33 @@ function Home(props) {
   /** @type (Array) */
   const listaDeProdutos = props.listaDeProdutos;
 
+
+    const [ordination, setOrdination] = useState("");
   
+
+  const onChangeOrdemProdutos = (event) =>{
+    setOrdination(event.target.value);
+  }
 
   return (
     <HomeArticle>
       <OrdenacaoSection>
-        <div>Quantidade de produtos:{listaDeProdutos.length}</div>
+        <div>Quantidade de produtos: {listaDeProdutos.length}</div>
         <div>
           <label>Ordenação</label>
-          <select>
-            <option value="">Opções</option>
+          <select
+          onChange={onChangeOrdemProdutos}
+          >
+            <option value="Crescente">Crescente</option>
+            <option value="Decrescente">Decrescente</option>
           </select>
         </div>
       </OrdenacaoSection>
 
       <HomeCards>
-        <ProductCard 
-        produto={listaDeProdutos[0]}
-        />
-         <ProductCard 
-        produto={listaDeProdutos[1]}
-        />
-         <ProductCard 
-        produto={listaDeProdutos[2]}
-        />
+        <ProductCard produto={listaDeProdutos[0]} />
+        <ProductCard produto={listaDeProdutos[1]} />
+        <ProductCard produto={listaDeProdutos[2]} />
       </HomeCards>
     </HomeArticle>
   );
