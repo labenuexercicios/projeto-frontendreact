7fcad214881faaf3292e08a34ad0df7a9ecfb051
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2023-06-18 17:28:44 -0300

    [function] apliquei Inputs controlados no select

--- LabeCommerce/src/App.jsx
+++ LabeCommerce/src/App.jsx
@@ -3,15 +3,26 @@ import Home from "./Components/ProductList/Home";
 import Cart from "./Components/ShoppingCart/Cart";
 import { Wrapper } from "./App.styled.jsx";
 import Product from "./assets/productList";
+import { useState } from "react";
 
 function App() {
   
+  const [minFilter, setMinFilter] = useState('')
+  const [maxFilter, setMaxFilter] = useState('')
+  const [searchFilter, setSearchFilter] = useState('')
+  const [cart, setCart] = useState('')
+  const [amount, setAmount] = useState('')
+  
+
+
+
+
   return (
     <>
       <Wrapper>
 
         <Filter />
-        <Home Produto={Product} />
+        <Home produto={Product} />
         <Cart />
         
 
