36b74d680d015440d7249dd3c82cf1f89405b7dc
Cristian91Assuncao <cristian.assuncao@hotmail.com>
2023-06-19 22:09:35 -0300

    [template] Criei a estrutura inicial de arquivos

--- projeto-frontendreact/src/App.js
+++ projeto-frontendreact/src/App.js
@@ -13,7 +13,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          
         >
           Learn React
         </a>
