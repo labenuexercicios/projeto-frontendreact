9ad82b12895f0f4b11d29cc9bfe84692204a86fe
Re-Santos <renata.prog@gmail.com>
2023-07-28 14:19:02 -0300

    Estiliza links

--- labecommerce/src/Components/Footer/Footer.js
+++ labecommerce/src/Components/Footer/Footer.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { FooterStyle, Base, SocialMedia } from "./footerStyle";
+import { FooterStyle, Base, SocialMedia,Help } from "./footerStyle";
 import {MdFacebook, MdWhatsapp} from 'react-icons/md';
 
 const Footer = () =>{
     return(
     <>
      <FooterStyle>
-     <help>
-        <p>Atendimento</p>
-        <p>Trocas e Devoluções</p>
-     </help>
+     <Help>
+        <Help href="https://www.google.com" target="_blank" rel="noopener noreferrer">Atendimento</Help>
+        <Help href="https://www.google.com" target="_blank" rel="noopener noreferrer">Trocas e Devoluções</Help>
+     </Help>
      {/* <pay>
         <p>Visa</p>
         <p>Master Card</p>
         <p>Elo</p>
      </pay> */}
-     <SocialMedia>
-         <p><MdFacebook size={28}/></p>
-         <p><MdWhatsapp size={28}/></p>
+     <SocialMedia> 
+         <SocialMedia href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" ><MdFacebook  size={28}/></SocialMedia>
+         <SocialMedia href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer" ><MdWhatsapp size={28}/></SocialMedia>
      </SocialMedia>
      </FooterStyle>
             
      <Base>
-         <p>Todos os direitos reservados © 2023</p><br/>
+         <p>Todos os direitos reservados © 2023</p>
          <p>Desenvolvido por: Renata Santos</p>
      </Base>
 
