61efb5f0cba18a165310a36c42c1a4e3cbc99352
henriquelbk <loubackhenrique123@gmail.com>
2023-08-06 15:22:53 -0300

    [configuração] Usei props para distribuir os dados do projeto

--- labecommerce/src/Components/ProductList/Home/Home.js
+++ labecommerce/src/Components/ProductList/Home/Home.js
@@ -1,20 +1,36 @@
+import { productList } from "../../../assets/productList";
 import ProductCard from "../ProductCard/ProductCard";
 import { HomeContainer } from "./HomeStyle";
 
-const Home = () => {
-    return (
-        <HomeContainer>
-            <header>
-            <p>Quantidade de Produtos:</p>
-            <p>Ordenação:</p>
-            </header>
-            <main>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            </main>
-        </HomeContainer>
-    )
-}
+const Home = (props) => {
+
+  const totalProducts = productList.length;
+
+  return (
+    <HomeContainer>
+      <header>
+        <p>Quantidade de Produtos: {totalProducts}</p>
+        <div id="order">
+          <p>Ordenação:</p>
+          <select>
+            <option value="opcao1">Crescente</option>
+            <option value="opcao2">Decrescente</option>
+            <option value="opcao3">Melhor Avaliação</option>
+          </select>
+        </div>
+      </header>
+      <main>
+        {productList.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            value={product.value}
+            imgUrl={product.imageUrl}
+          />
+        ))}
+      </main>
+    </HomeContainer>
+  );
+};
 
 export default Home;
--- labecommerce/src/Components/ProductList/Home/HomeStyle.js
+++ labecommerce/src/Components/ProductList/Home/HomeStyle.js
@@ -1,22 +1,28 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const HomeContainer = styled.div`
-    height: 100vh;
-    border: 1px solid black;
+  height: 100vh;
+  border: 1px solid black;
 
-    header {
-        display:flex;
-        justify-content: space-between;
-        padding-right: 1vw;
-        padding-left: 1vw;
-        padding-top: 2vw;
-    }
+  header {
+    display: flex;
+    justify-content: space-between;
+    padding-right: 1vw;
+    padding-left: 1vw;
+    padding-top: 2vw;
+  }
 
-    main {
-        display: flex;
-        gap: 5vw;
-        padding-top: 6vw;
-        align-items: center;
-        justify-content: center;
-    }
-`
+  #order {
+    display: flex; 
+    align-items: center;
+    gap: .5vw;
+  }
+
+  main {
+    display: flex;
+    gap: 5vw;
+    padding-top: 6vh;
+    align-items: center;
+    justify-content: center;
+  }
+`;
