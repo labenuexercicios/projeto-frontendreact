b6935c63853f2b0235e3388d0cef0d80e6948162
henriquelbk <loubackhenrique123@gmail.com>
2023-08-09 21:48:20 -0300

    indo

--- labecommerce/src/App.js
+++ labecommerce/src/App.js
@@ -1,19 +1,26 @@
-import Filters from './Components/Filters/Filters';
-import Home from './Components/ProductList/Home/Home';
-import Cart from './Components//ShoppingCart/Cart/Cart';
-import { GlobalStyle } from './GlobalStyle';
-import { productList } from './assets/productList';
+import Filters from "./Components/Filters/Filters";
+import Home from "./Components/ProductList/Home/Home";
+import Cart from "./Components//ShoppingCart/Cart/Cart";
+import { GlobalStyle } from "./GlobalStyle";
+import { productList } from "./assets/productList";
+import { useState } from "react";
 
 function App() {
+  const [minFilter, setMinFilter] = useState();
+  const [maxFilter, setMaxFilter] = useState();
+  const [searchFilter, setSearchFilter] = useState();
+  const [cart, setCart] = useState();
+  const [amount, setAmount] = useState();
+
   return (
     <>
-    <GlobalStyle />
+      <GlobalStyle />
 
-    <main>
-    <Filters />
-    <Home productList={productList}/>
-    <Cart />
-    </main>
+      <main>
+        <Filters />
+        <Home productList={productList} />
+        <Cart />
+      </main>
     </>
   );
 }
