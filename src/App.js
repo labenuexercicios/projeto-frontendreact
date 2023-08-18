242843ed258f5b298b0218bdfcfc7904994572f3
sivilela <sivilela.s@gmail.com>
2023-08-18 16:39:41 -0300

    Criei a estrutura inicial de arquivos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./modulo_scss/App.scss"
+
+
+function App() {
+  
+  return (
+    <>
+      <header className="cabecalho">Cabeçalho</header>
+
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
