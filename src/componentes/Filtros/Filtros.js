228e50c48f8cee6038405bd16dd055c1ff976fd8
sivilela <sivilela.s@gmail.com>
2023-08-27 22:25:18 -0300

    [configuração] Estruturei o fluxo de dados usando props

--- src/componentes/Filtros/Filtros.js
+++ src/componentes/Filtros/Filtros.js
@@ -1,24 +1,48 @@
 import { FiltroContainer, BtFiltro } from "./styleFiltros";
 
-
-function Filtros() {
+function Filtros(props) {
+  console.log(props);
   return (
     <FiltroContainer>
       <h1>Filtros</h1>
       <label>
         Filtro mínimo:
-        <br/>
-        <input type="number" />
+        <br />
+        <input
+          type="number"
+          value={props.minFilter}
+          onChange={(event) => {
+            if (event.target.value < 0) {
+              props.setMinFilter(0);
+              return;
+            }
+            props.setMinFilter(event.target.value);
+          }}
+        />
       </label>
       <label>
         Filtro máximo:
-        <br/>
-        <input type="number" />
+        <br />
+        <input
+          type="number"
+          value={props.maxFilter}
+          onChange={(event) => {
+            if (event.target.value < 0) {
+              props.setMaxFilter(0);
+              return;
+            }          
+            props.setMaxFilter(event.target.value)
+          }}
+        />
       </label>
       <label>
         Buscar por nome:
-        <br/>
-        <input type="text" />
+        <br />
+        <input
+          type="text"
+          value={props.searchFilter}
+          onChange={(event) => props.setSearchFilter(event.target.value)}
+        />
       </label>
       <BtFiltro>Buscar</BtFiltro>
     </FiltroContainer>
